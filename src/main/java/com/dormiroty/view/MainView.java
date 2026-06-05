0c2d2555504d5bd4d0c3d1868b47aeb6dfb0aa94
24130317@st.hcmuaf.edu.vn <24130317@st.hcmuaf.edu.vn>
2026-06-06 01:39:56 +0700

    Cap nhat MainView

--- src/main/java/com/dormiroty/view/MainView.java
+++ src/main/java/com/dormiroty/view/MainView.java
@@ -1,9 +1,10 @@
 package com.dormiroty.view;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 public class MainView extends JFrame {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public MainView() {
         setTitle("Quản lý KTX");
@@ -11,9 +12,22 @@ public class MainView extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        add(new SinhVienPanel());
+        JTabbedPane tabs = new JTabbedPane();
+
+        tabs.addTab("Sinh viên", new SinhVienPanel());
+        tabs.addTab("Nhân viên", new NhanVienPanel());
+        tabs.addTab("Phòng", new PhongPanel());
+        tabs.addTab("Loại phòng", new LoaiPhongPanel());
+        tabs.addTab("Hợp đồng", new HopDongPanel());
+        tabs.addTab("Vi phạm", new ViPhamPanel());
+        tabs.addTab("Tòa nhà", new ToaNhaPanel());
+        tabs.addTab("Diện chính sách", new DienChinhSachPanel());
+
+        add(tabs);
     }
+
     public static void main(String[] args) {
         new MainView().setVisible(true);
     }
 }
+
